bc1db8a55f0c1087a7fe9ea81ad84b9f7502fc71
Raira <rairacarvalho@live.com>
2021-01-31 18:44:48 -0300

    commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name="tap-estoca,
+    name="tap-estoca",
     version="0.1.0",
     description="Singer.io tap for extracting data",
     author="Raira",
